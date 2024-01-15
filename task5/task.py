b17089356dad67f7dc635780b9f950af327cd91f
starman-in-the-sky <ms.practice@yandex.ru>
2024-01-15 18:20:08 +0300

    task5 recreation

--- task5/task.py
+++ task5/task.py
@@ -1,71 +1,122 @@
-import numpy as np
 import json
-from collections.abc import Sequence
+from typing import Any
+import numpy as np
+
+class WeightedOrder:
+    count: int
+    order_array: np.array
+    order_map: dict
+    index_to_key: dict
+
+    def __init__(self, input_json: str) -> None:
+        items = json.loads(input_json)
+        self.order_map = {}
+        current_weight = 1.0
+        for item in items:
+            if isinstance(item, list):
+                group_weight = current_weight + (len(item) - 1) / 2
+                for sub_item in item:
+                    self.order_map[sub_item] = group_weight
+                current_weight += len(item)
+            else:
+                self.order_map[item] = current_weight
+                current_weight += 1
+        ordered_keys = sorted(self.order_map)
+        self.index_to_key = {ordered_keys.index(key): key for key in ordered_keys}
+        self.count = len(self.order_map)
+        self.order_array = np.zeros((self.count, self.count))
+        for i in range(self.count):
+            for j in range(self.count):
+                if self.order_map[self.index_to_key[i]] <= self.order_map[self.index_to_key[j]]:
+                    self.order_array[i, j] = 1
 
 
-# Рекурсивно разворачивает вложенные последовательности
-def expand_sequence(seq):
-    expanded = []
-    for item in seq:
-        if isinstance(item, Sequence) and not isinstance(item, str):
-            expanded.extend(expand_sequence(item))
+def identify_discrepancies(order1: WeightedOrder, order2: WeightedOrder) -> list:
+    combined_orders = np.logical_and(order1.order_array, order2.order_array)
+    transposed = np.transpose(combined_orders)
+    discrepancies = np.logical_or(combined_orders, transposed)
+    output = []
+    for i in range(order1.count):
+        for j in range(i + 1, order1.count):
+            if not discrepancies[i, j]:
+                if not output:
+                    output.append({order1.index_to_key[i], order1.index_to_key[j]})
+                else:
+                    match_found = False
+                    for element in output:
+                        if order1.index_to_key[i] in element:
+                            element.add(order1.index_to_key[j])
+                            match_found = True
+                            break
+                        elif order1.index_to_key[j] in element:
+                            element.add(order1.index_to_key[i])
+                            match_found = True
+                            break
+                    if not match_found:
+                        output.append({order1.index_to_key[i], order1.index_to_key[j]})
+
+    return [sorted(list(group)) for group in output]
+
+def compare_elements(elem1: Any, elem2: Any, map1: dict, map2: dict) -> int:
+    if (map1[elem1] > map1[elem2] and map2[elem1] > map2[elem2]) \
+            or (map1[elem1] > map1[elem2] and map2[elem1] == map2[elem2]) \
+            or (map1[elem1] == map1[elem2] and map2[elem1] > map2[elem2]):
+        return 1
+    elif (map1[elem1] < map1[elem2] and map2[elem1] < map2[elem2]) \
+            or (map1[elem1] < map1[elem2] and map2[elem1] == map2[elem2]) \
+            or (map1[elem1] == map1[elem2] and map2[elem1] < map2[elem2]):
+        return -1
+    return 0
+
+def create_combined_order(order1: WeightedOrder, order2: WeightedOrder, base: list) -> list:
+    map1, map2 = order1.order_map, order2.order_map
+    combined_order = [base[0]] if base else []
+    for group in base[1:]:
+        elem = group[0]
+        for idx, existing_group in enumerate(combined_order):
+            existing_elem = existing_group[0] if isinstance(existing_group, list) else existing_group
+            order_comparison = compare_elements(elem, existing_elem, map1, map2)
+            if order_comparison == 1:
+                if idx == len(combined_order) - 1:
+                    combined_order.append(group)
+                    break
+            elif order_comparison == -1:
+                combined_order.insert(idx, group)
+                break
+
+    remaining_keys = list(order1.index_to_key.values())
+    for grouped_key in combined_order:
+        if isinstance(grouped_key, list):
+            for key in grouped_key:
+                remaining_keys.remove(key)
         else:
-            expanded.append(item)
-    return expanded
+            remaining_keys.remove(grouped_key)
 
+    for single_key in remaining_keys:
+        for idx, grouped_key in enumerate(combined_order):
+            comparison_elem = grouped_key[0] if isinstance(grouped_key, list) else grouped_key
+            comparison_result = compare_elements(single_key, comparison_elem, map1, map2)
+            if comparison_result == 1:
+                if idx == len(combined_order) - 1:
+                    combined_order.append(single_key)
+                    break
+            elif comparison_result == -1:
+                combined_order.insert(idx, single_key)
+                break
+            elif comparison_result == 0:
+                if isinstance(grouped_key, list):
+                    grouped_key.append(single_key)
+                else:
+                    combined_order[idx] = [comparison_elem, single_key]
+                break
 
-# Строит матрицу связей для массива
-def create_link_matrix(data):
-    expanded_data = expand_sequence(data)
-    matrix_size = len(expanded_data)
-    link_matrix = np.zeros((matrix_size, matrix_size), dtype=int)
+    return combined_order
 
-    for i, val in enumerate(expanded_data):
-        index = int(val) - 1
-        for j in range(i + 1):
-            link_matrix[int(expanded_data[j]) - 1, index] = 1
-
-    for subgroup in data:
-        if isinstance(subgroup, Sequence) and not isinstance(subgroup, str):
-            for val in subgroup:
-                index = int(val) - 1
-                for other_val in subgroup:
-                    link_matrix[index, int(other_val) - 1] = 1
-                    link_matrix[int(other_val) - 1, index] = 1
-
-    return link_matrix
-
-
-# Вычисляет основу связей между двумя матрицами
-def calculate_basis(matrix1, matrix2):
-    transposed_matrix1 = matrix1.T
-    transposed_matrix2 = matrix2.T
-
-    combined_relation = np.logical_or(np.multiply(matrix1, matrix2),
-                                      np.multiply(transposed_matrix1, transposed_matrix2))
-
-    basis = []
-    for i in range(len(combined_relation)):
-        for j in range(i, len(combined_relation[i])):
-            if not combined_relation[i][j]:
-                basis.append([str(i + 1), str(j + 1)])
-
-    return basis
-
-
-# Преобразует JSON-строку в объект
-def parse_json(input_str):
-    return json.loads(input_str)
-
-
-# Основная функция задачи
-def task(json_str1, json_str2):
-    data1 = parse_json(json_str1)
-    data2 = parse_json(json_str2)
-
-    matrix1 = create_link_matrix(data1)
-    matrix2 = create_link_matrix(data2)
-
-    return calculate_basis(matrix1, matrix2)
-
+def execute_task(json_input1: str, json_input2: str) -> str:
+    order1 = WeightedOrder(json_input1)
+    order2 = WeightedOrder(json_input2)
+    assert order1.index_to_key == order2.index_to_key, 'Key lists in the orders do not match.'
+    base_discrepancies = identify_discrepancies(order1, order2)
+    final_order = create_combined_order(order1, order2, base_discrepancies)
+    return json.dumps(final_order, separators=(',', ':'))
 
